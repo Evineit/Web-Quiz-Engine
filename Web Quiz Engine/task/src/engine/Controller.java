dc28ebf6a01c28f8d29fb9858d7b39163d018c28
Kevin R <seidaikevin@gmail.com>
2020-07-05 16:54:21 -0500

    Add quiz, solve quizzes, get all quizzes implemented

--- Web Quiz Engine/task/src/engine/Controller.java
+++ Web Quiz Engine/task/src/engine/Controller.java
@@ -1,20 +1,22 @@
 package engine;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @RestController
 public class Controller {
-    private Quiz quiz;
+    private ArrayList<Quiz> quizzes = new ArrayList<>();
 
     public Controller() {
     }
 
     @GetMapping("/api/quiz")
-    public ResponseEntity<String> getQuiz(){
+    public ResponseEntity<String> getQuiz() {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
                 .body("{\n" +
@@ -24,10 +26,78 @@ public class Controller {
                         "}\n");
     }
 
+    @GetMapping("/api/quizzes/{id}")
+    public ResponseEntity<String> getQuizById(@PathVariable int id) {
+        Quiz quiz = quizzes.get(id - 1);
+        if (quiz != null) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
+                    .body("{\n" +
+                            "  \"id\": " + id + 1 + ",\n" +
+                            "  \"title\": \"" + quiz.getTitle() + "\",\n" +
+                            "  \"text\": \"" + quiz.getText() + "\",\n" +
+                            "  \"options\": " + quiz.getOptions().toString() + "\n" +
+                            "}");
+        } else {
+            return ResponseEntity.notFound()
+                    .build();
+        }
+    }
+
+    @GetMapping("/api/quizzes")
+    public ResponseEntity<String> getQuizzes() {
+        String[] responseBody = new String[quizzes.size()];
+        for (int i = 0; i < quizzes.size(); i++) {
+            Quiz quiz = quizzes.get(i);
+            responseBody[i] = "{\n" +
+                    "  \"id\": " + i + 1 + ",\n" +
+                    "  \"title\": \"" + quiz.getTitle() + "\",\n" +
+                    "  \"text\": \"" + quiz.getText() + "\",\n" +
+                    "  \"options\": " + quiz.getOptions().toString() + "\n" +
+                    "}";
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
+                .body(Arrays.toString(responseBody));
+    }
+
     @PostMapping("/api/quiz")
     ResponseEntity<String> answerQuiz(@RequestParam("answer") int answer) {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
-                .body(quiz.saveAnswer(answer));
+                .body(Quiz.saveAnswer(answer));
+    }
+
+    @PostMapping(value = "/api/quizzes", consumes = "application/json")
+    ResponseEntity<String> newQuiz(@RequestBody Quiz quiz) {
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
+                .body(addQuiz(quiz));
+    }
+
+    @PostMapping(value = "/api/quizzes/{id}/solve")
+    ResponseEntity<String> solveQuiz(@PathVariable int id, @RequestParam int answer) {
+        try {
+            Quiz quiz = quizzes.get(id);
+            if (quiz.getAnswer() == answer) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON))
+                        .body(quiz.solveQuiz(answer));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.notFound()
+                .build();
+    }
+
+    private String addQuiz(Quiz quiz) {
+        quizzes.add(quiz);
+        return "{\n" +
+                "  \"id\": " + quizzes.size() + ",\n" +
+                "  \"title\": \"" + quiz.getTitle() + "\",\n" +
+                "  \"text\": \"" + quiz.getText() + "\",\n" +
+                "  \"options\": " + quiz.getOptions().toString() + "\n" +
+                "}";
     }
 }
--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,17 +1,27 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class Quiz {
     private String title;
     private String text;
-    private String[] options;
+    private ArrayList<String> options = new ArrayList<>();
+    private int answer;
 
     public Quiz() {
     }
 
-    public Quiz(String title, String text, String[] options) {
+    public Quiz(String title, String text, ArrayList<String> options) {
         this.title = title;
         this.text = text;
         this.options = options;
+    }
+
+    public Quiz(String title, String text, ArrayList<String> options, int answer) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
     }
 
     public String getTitle() {
@@ -30,18 +40,34 @@ public class Quiz {
         this.text = text;
     }
 
-    public String[] getOptions() {
+    public ArrayList<String> getOptions() {
         return options;
     }
 
-    public void setOptions(String[] options) {
+    public void setOptions(ArrayList<String> options) {
         this.options = options;
     }
 
-    public static String saveAnswer(int answer) {
-        if (answer==2){
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public String solveQuiz(int answer) {
+        if (answer == this.answer) {
             return "{\"success\":true,\"feedback\":\"Congratulations, you're right!\"}";
-        }else {
+        } else {
+            return "{\"success\":false,\"feedback\":\"Wrong answer! Please, try again.\"}\n";
+        }
+    }
+
+    public static String saveAnswer(int answer) {
+        if (answer == 2) {
+            return "{\"success\":true,\"feedback\":\"Congratulations, you're right!\"}";
+        } else {
             return "{\"success\":false,\"feedback\":\"Wrong answer! Please, try again.\"}\n";
         }
     }
